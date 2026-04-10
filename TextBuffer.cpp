7345d59ce2b5bec87ae7980ad42d7b302e6899b2
richardson-liou <richardson.liou@gmail.com>
2026-04-10 18:49:58 -0400

    Passed public tests TextBuffer.cpp

--- TextBuffer.cpp
+++ TextBuffer.cpp
@@ -30,7 +30,7 @@ bool TextBuffer::backward() {
 
     if (*cursor == '\n') {
         --row;
-        compute_column();
+        column = compute_column();
     } 
     else {
         --column;
@@ -40,74 +40,124 @@ bool TextBuffer::backward() {
 }
 
 void TextBuffer::insert(char c) {
-  data.insert(cursor, c);
+    data.insert(cursor, c);
 
-  if (c == '\n') {
-    ++row;
-    column = 0;
-  } 
-  else {
-    ++column;
-  }
+    if (c == '\n') {
+        ++row;
+        column = 0;
+    } 
+    else {
+        ++column;
+    }
 
-  ++index;
+    ++index;
 }
 
 bool TextBuffer::remove() {
-  if (cursor == data.end()) return false;
+    if (cursor == data.end()) return false;
 
-  char c = *cursor;
-  cursor = data.erase(cursor);
+    char c = *cursor;
+    cursor = data.erase(cursor);
 
-  if (c == '\n') {
-    --row;
-    compute_column();
-  } 
+    if (c == '\n') {
+        --row;
+        column = compute_column();
+    } 
 
-  return true;
+    return true;
 }
 
 void TextBuffer::move_to_row_start() {
-  while (cursor != data.begin()) {
-    --cursor;
-    if (*cursor == '\n') {
-      ++cursor;
-      break;
+    while (cursor != data.begin()) {
+        --cursor;
+        --index;
+        if (*cursor == '\n') {
+            ++cursor;
+            ++index;
+            break;
+        }
     }
-  }
-  column = 0;
+    column = 0;
 }
 
 void TextBuffer::move_to_row_end() {
-  while (cursor != data.end() && *cursor != '\n') {
-    ++cursor;
-    ++column;
-  }
+    while (cursor != data.end() && *cursor != '\n') {
+        ++cursor;
+        ++column;
+        ++index;
+    }
+}
+
+void TextBuffer::move_to_column(int new_column){
+    move_to_row_start();
+    
+    while (column < new_column && cursor != data.end() && *cursor != '\n') {
+        forward();
+    }
+}
+
+bool TextBuffer::up(){
+    if (row == 1){
+        return false;
+    }
+
+    int curr_col = column;
+
+    move_to_row_start();
+  
+    if (cursor != data.begin()) {
+        backward();
+        
+        move_to_column(curr_col);
+    }
+    return true;
+}
+
+bool TextBuffer::down(){
+    int curr_col = column;
+
+    move_to_row_end();
+
+    if (cursor != data.end() && *cursor == '\n') {
+        forward();
+    }
+
+    move_to_column(curr_col);
+
+    return true;
 }
 
 bool TextBuffer::is_at_end() const {
-  return cursor == data.end();
+    return cursor == data.end();
 }
 
 char TextBuffer::data_at_cursor() const {
-  assert(cursor != data.end());
-  return *cursor;
-}
-
-bool TextBuffer::is_at_end() const {
-  return cursor == data.end();
+    assert(cursor != data.end());
+    return *cursor;
 }
 
 int TextBuffer::get_row() const {
-  return row;
+    return row;
 }
 
 int TextBuffer::get_column() const {
-  return column;
+    return column;
 }
 
 int TextBuffer::get_index() const {
-  return index;
+    return index;
+}
+
+int TextBuffer::size() const{
+    return data.size();
+}
+
+std::string TextBuffer::stringify() const {
+    string result;
+    for (char c : data) {
+        result += c;
+    }
+    return result;
 }
 
 int TextBuffer::compute_column() const{
